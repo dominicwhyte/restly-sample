1a8e7341ef55bb6ba0720f68aa683bccecaa9126
dominicwhyte <dwhyte@alumni.princeton.edu>
2022-02-18 21:49:28 -0700

    progress

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import restly from 'my-restly-demo-sdk'
 // @ts-ignore
 // In prod, RESTLY_KEY is set to the production key
-restly.authenticate(process.env.RESTLY_KEY)
+// restly.authenticate(process.env.RESTLY_KEY)
 
 
 /**
@@ -31,5 +31,19 @@ export const getItem = (parameters: {
     // @ts-ignore
     const data = await restly.GoogleSheet.addRow('my-gsheet-service', { data: [{ name: parameters.name }] })
     return data
+}
+
+
+/**
+ * @restly A test endpoint
+ * @method GET
+ * @path /testing
+ */
+export const getTesting = (parameters: {
+    // The name of the item to get 
+    name: string
+}) => {
+    const test = `${name} hello`
+    return test
 }
 
