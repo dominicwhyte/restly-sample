bff5fa1d93af2429a7f888e8f2e4a8aacb599c1b
dominicwhyte <dwhyte@alumni.princeton.edu>
2022-02-25 16:22:06 -0600

    update

--- build/detailedExample.js
+++ build/detailedExample.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = exports.onRequestAccess = void 0;
+exports.getWaitlist = exports.onRequestAccess = void 0;
+// @ts-nocheck
 const restly_1 = require("restly");
 const restly = (0, restly_1.default)('a5b5c56f2de8a865961eb6f6066dfd34574121a6e0f1f8ff');
 const googlesheet = restly.GoogleSheets('googlesheets-1');
+const slack = restly.Slack('restly-slack');
+const sendgrid = restly.Sendgrid('restly-slack');
 const mySheetId = '19SgoB5sMKxeSnQ0eQbTwHyJAnQ1WPCk0tLf0iMB8lkg';
+const campaignId = '12312';
 /**
  * @restly Handle access request to the Restly beta!!
  * @method POST
@@ -12,16 +16,19 @@ const mySheetId = '19SgoB5sMKxeSnQ0eQbTwHyJAnQ1WPCk0tLf0iMB8lkg';
  */
 const onRequestAccess = async ({ email }) => {
     await googlesheet.addRow({ sheetId: mySheetId, value: { email } });
+    await slack.message({ channel: '#waitlist', message: `${email} joined the waitlist` });
+    await sendgrid.sendEmail({ email, campaignId });
     return;
 };
 exports.onRequestAccess = onRequestAccess;
 /**
- * @restly Simple demo endpoint
+ * @restly Get list of emails in the Restly beta
  * @method GET
- * @path /test
+ * @path /waitlist
  */
-const test = async ({ input }) => {
-    return `You gave input: ${input}`;
+const getWaitlist = async () => {
+    const waitlistEntries = await googlesheet.read({ sheetId: mySheetId });
+    return waitlistEntries;
 };
-exports.test = test;
-//# sourceMappingURL=index.js.map
+exports.getWaitlist = getWaitlist;
+//# sourceMappingURL=detailedExample.js.map
--- detailedExample.ts
+++ detailedExample.ts
@@ -1,10 +1,14 @@
+// @ts-nocheck
 import Restly from 'restly';
 
 
 const restly = Restly('a5b5c56f2de8a865961eb6f6066dfd34574121a6e0f1f8ff');
 const googlesheet = restly.GoogleSheets('googlesheets-1');
+const slack = restly.Slack('restly-slack');
+const sendgrid = restly.Sendgrid('restly-slack');
 
 const mySheetId = '19SgoB5sMKxeSnQ0eQbTwHyJAnQ1WPCk0tLf0iMB8lkg'
+const campaignId = '12312'
 
 /**
  * @restly Handle access request to the Restly beta!!
@@ -18,19 +22,22 @@ export const onRequestAccess = async ({email}: {
 
     await googlesheet.addRow({ sheetId: mySheetId, value: { email } });
 
+    await slack.message({ channel: '#waitlist', message: `${email} joined the waitlist` });
+
+    await sendgrid.sendEmail({email, campaignId})
+
     return
 }
 
-/**
- * @restly Simple demo endpoint
- * @method GET
- * @path /test
- */
-export const test = async ({input}: {
-    // Email requesting access
-    input: string
-}) => {
-    return `You gave input: ${input}`
-}
 
+/**
+ * @restly Get list of emails in the Restly beta
+ * @method GET
+ * @path /waitlist
+ */
+export const getWaitlist = async () => {
+
+    const waitlistEntries = await googlesheet.read({sheetId: mySheetId})
+    return waitlistEntries
+}
 
