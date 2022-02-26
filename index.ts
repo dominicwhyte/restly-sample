f7c6a27988d607d2dc1a14761853eef2f273a81a
dominicwhyte <dwhyte@alumni.princeton.edu>
2022-02-26 13:50:09 -0600

    update

--- index.ts
+++ index.ts
@@ -23,15 +23,3 @@ export const onRequestAccess = async ({email}: {
     return
 }
 
-/**
- * @restly Testing
- * @method GET
- * @path /test
- */
-export const getTest = async ({email}: {
-    // Email requesting access
-    email: string
-}) => {
-    return `${email} hello`
-}
-
