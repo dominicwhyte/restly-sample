aefc08704ef4c7307f0cd15d9d9c7434cd0daccb
dominicwhyte <dwhyte@alumni.princeton.edu>
2022-02-28 13:17:47 -0600

    update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeAccess = exports.grantAccess = exports.getWaitlist = exports.onRequestAccess = void 0;
+exports.onRequestAccess = void 0;
 const restly_1 = require("restly");
 const restly = (0, restly_1.default)('7bdba6026504ca5027274375cfde5def0152973372eb85c7');
 const googlesheet = restly.GoogleSheets('googlesheets-1');
@@ -15,34 +15,4 @@ const onRequestAccess = async ({ email }) => {
     return;
 };
 exports.onRequestAccess = onRequestAccess;
-/**
- * @restly Get waitlist entries
- * @method GET
- * @path /waitlist
- */
-const getWaitlist = async ({ email }) => {
-    const response = await googlesheet.read({ sheetId: mySheetId });
-    return response;
-};
-exports.getWaitlist = getWaitlist;
-/**
- * @restly Grant access to waitlist
- * @method POST
- * @path /grantAccess
- */
-const grantAccess = async ({ email }) => {
-    const response = await googlesheet.read({ sheetId: mySheetId });
-    return response;
-};
-exports.grantAccess = grantAccess;
-/**
- * @restly Remove from waitlist
- * @method DELETE
- * @path /removeAccess
- */
-const removeAccess = async ({ email }) => {
-    const response = await googlesheet.read({ sheetId: mySheetId });
-    return response;
-};
-exports.removeAccess = removeAccess;
 //# sourceMappingURL=index.js.map
