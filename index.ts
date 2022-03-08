613141c471676cef99dab16a6a063853404f7297
dominicwhyte <dwhyte@alumni.princeton.edu>
2022-03-08 12:28:20 -0600

    update

--- index.ts
+++ index.ts
@@ -22,14 +22,5 @@ export const onRequestAccess = async ({email}: {
     return
 }
 
-/**
- * @restly Get the waitlist
- * @method GET
- * @path /waitlist
- */
- export const getWaitlist = async () => {
-    const response = await googlesheet.read({sheetId: mySheetId})
 
-    return response
-}
 
