08d45d0c91bd7f53a19b2cf708a3b9862c4fa493
dominicwhyte <dwhyte@alumni.princeton.edu>
2022-03-08 10:37:58 -0600

    update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getWaitlist = exports.onRequestAccess = void 0;
+exports.onRequestAccess = void 0;
 const restly_1 = require("restly");
 const restly = (0, restly_1.default)('7bdba6026504ca5027274375cfde5def0152973372eb85c7');
 const googlesheet = restly.GoogleSheets('googlesheets-2');
@@ -15,14 +15,4 @@ const onRequestAccess = async ({ email }) => {
     return;
 };
 exports.onRequestAccess = onRequestAccess;
-/**
- * @restly Get all the entries in the waitlist
- * @method GET
- * @path /waitlist
- */
-const getWaitlist = async () => {
-    const response = await googlesheet.read({ sheetId: mySheetId });
-    return response;
-};
-exports.getWaitlist = getWaitlist;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -22,13 +22,3 @@ export const onRequestAccess = async ({email}: {
     return
 }
 
-/**
- * @restly Get all the entries in the waitlist
- * @method GET
- * @path /waitlist
- */
-export const getWaitlist = async () => {
-    const response = await googlesheet.read({sheetId: mySheetId})
-    return response
-}
-
